c871aef1e4f184d5d64032bfb0407460456db1c7
Alex Pages <alexpagesandreu@gmail.com>
2025-01-27 14:07:55 +0900

    Refactor and organize code

--- src/main/java/com/apa/clipfarmer/ClipFarmer.java
+++ src/main/java/com/apa/clipfarmer/ClipFarmer.java
@@ -1,14 +1,11 @@
 package com.apa.clipfarmer;
 
 import com.apa.clipfarmer.logic.ClipFarmerLogic;
-import com.apa.clipfarmer.logic.TwitchLogic;
 import com.apa.clipfarmer.model.ClipFarmerArgs;
 import com.apa.clipfarmer.model.StreamerNameEnum;
 import com.beust.jcommander.JCommander;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,22 +33,25 @@ public class ClipFarmer {
         final ClipFarmerArgs clipFarmerArgs;
 
         try {
-            System.out.println(TwitchLogic.getOAuthToken());
-
             ClipFarmerArgs.Builder clipFarmerArgsBuilder = new ClipFarmerArgs.Builder();
             new JCommander(clipFarmerArgsBuilder).parse(args);
             clipFarmerArgs = clipFarmerArgsBuilder.build();
-            if (StringUtils.isEmpty(clipFarmerArgs.getStreamerNameEnum().getName())) {
-                throw new Exception("Streamer name is invalid");
-            }
-
         } catch (Exception e) {
-            LOGGER.warn("Twitch streamer is not present in list or was null. Error: {}", e.getMessage());
+            LOGGER.warn("Invalid arguments: {}", e.getMessage());
             return;
         }
 
         StreamerNameEnum streamerNameEnum = clipFarmerArgs.getStreamerNameEnum();
-        //TODO implement logic
-        System.out.println("Hello world!");
+        try {
+            if (StreamerNameEnum.INVALID.equals(streamerNameEnum)) {
+                LOGGER.warn("Twitch streamer is not present in list or was null");
+                return;
+            }
+            //TODO implement logic
+            System.out.println("Executing ClipFarmer logic for streamer: " + streamerNameEnum.getName());
+
+        } catch (Exception e) {
+
+        }
     }
 }
